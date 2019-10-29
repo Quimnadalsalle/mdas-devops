7cbf95f991192fc857d85a3e7891b810368b091e
paulopez78 <pau.lopez.78@gmail.com>
2019-10-29 21:58:08 +0100

    siguiente clase

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
-#  MDAS Devops tools: Scripting, Pipelines,  Docker, Kubernetes
+# MDAS Devops tools: Scripting, Pipelines,  Docker, Kubernetes
 
 ## **Cross Platform Setup (Windows, Mac or Linux)**
 
-### Download and install:
+### Download and install
+
 * [Visual Studio Code](https://code.visualstudio.com/download)
-  * Plugins: Go, Python, Docker 
+  * Plugins: Go, Python, Docker
 * [Git](https://git-scm.com/downloads)
 * [Docker](https://www.docker.com/products/docker-desktop)
 * [Golang](https://golang.org/dl/)
 * [Python](https://www.python.org/downloads/)
 * [jq](https://stedolan.github.io/jq/download/)
 
-### Signup for:
+### Signup for
+
 * [Dockerhub](https://hub.docker.com)
 * [Github](https://github.com)
 
+<<<<<<< HEAD
 # Indice para corregir:
 ## Ejercicio 1 - Pipelines
 test_pipeline.py (master)
@@ -23,3 +26,8 @@ Todavía en desarrollo en la rama: pipeline_docker_alpine
 Archivos afectados:
  Carpetas: src/votingapp, Test
  Archivos: pipeline-docker.sh
+=======
+### Pull most popular images
+
+Pull docker images running the script `pull.sh`
+>>>>>>> 2b90b58... siguiente clase
--- pipeline.sh
+++ pipeline.sh
@@ -83,14 +83,82 @@ retry test      #Arriba estara la funcion retry.
 set -e
 
 # install deps
-go get github.com/gorilla/websocket
-go get github.com/labstack/echo
+deps(){
+    go get github.com/gorilla/websocket
+    go get github.com/labstack/echo
+}
 
 # cleanup
-rm -rf build
+cleanup(){
+    rm -rf build || true
+    pkill votingapp || ps aux | grep votingapp | awk {'print $2'} | head -1 | xargs kill -9
+}
 
 # build 
+<<<<<<< HEAD
 mkdir build
 go build -o ./build ./src/votingapp 
 cp -r ./src/votingapp/ui ./build
 >>>>>>> c3e7793... super simple pipeline
+=======
+build(){
+    mkdir build
+    go build -o ./build ./src/votingapp 
+    cp -r ./src/votingapp/ui ./build
+
+    pushd build
+    ./votingapp &
+    popd
+}
+
+retry(){
+    n=0
+    interval=5
+    retries=3
+    $@ && return 0
+    until [ $n -ge $retries ]
+    do
+        n=$[$n+1]
+        echo "Retrying...$n of $retries, wait for $interval seconds"
+        sleep $interval
+        $@ && return 0
+    done
+
+    return 1
+}
+
+# test
+test() {
+    votingurl='http://localhost/vote'
+    curl --url  $votingurl \
+        --request POST \
+        --data '{"topics":["dev", "ops"]}' \
+        --header "Content-Type: application/json" 
+
+    curl --url $votingurl \
+        --request PUT \
+        --data '{"topic": "dev"}' \
+        --header "Content-Type: application/json" 
+    
+    winner=$(curl --url $votingurl \
+        --request DELETE \
+        --header "Content-Type: application/json" | jq -r '.winner')
+
+    echo "Winner IS "$winner
+
+    expectedWinner="dev"
+
+    if [ "$expectedWinner" == "$winner" ]; then
+        echo 'TEST PASSED'
+        return 0
+    else
+        echo 'TEST FAILED'
+        return 1
+    fi
+}
+
+deps
+cleanup
+build
+retry test
+>>>>>>> 2b90b58... siguiente clase
