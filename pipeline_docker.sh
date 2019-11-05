37ae71f605f75f76bbab54b771a1809dce903bff
Quim Nadal <quim.nadal@students.salle.url.edu>
2019-11-05 19:03:09 +0100

    New class, save data from the other one

--- pipeline_docker.sh
+++ pipeline_docker.sh
@@ -1,8 +1,4 @@
 #!/bin/bash         
-#Pones el hint para que ejecuten el script con este shell (puedes poner sh x ejemplo)
-
-#set -e                              #que el script pare cuando hay un error
-#set +e                              #el script sigue aunque haya errores
 
 deps(){
     go get github.com/gorilla/websocket #Dependencia para que funcione el websocket
@@ -14,39 +10,48 @@ deps(){
 cleanup(){
     docker rm -f myvotingapp
     rm -rf build                        #Por si ya tenemos el directorio
-    #pkill votingapp || ps aux | grep votingapp | head -1 | awk { 'print $1' } | xargs kill -9    #matas el proceso (sh) por si te lo habias dejado abierto
-    #No hace falta matar el proceso, pero si debemos eliminar el container anterior
 }
 
 #Build
 build(){
     mkdir build                                     #Carpeta para guardar la config
-    go build -o ./build ./src/votingapp || exit 1   #Guardas la compilación en build y si va mal se termina la ejecucion
+    go build -o ./build ./src/votingapp             #Guardas la compilación en build
     cp -r ./src/votingapp/ui ./build                #Copiamos los archivos no go
-
-    #pushd build                                     #Cambias de contexto -> es como un cd hasta que haces popd
-    #./votingapp &                                   #Con el & ejecutas en background
-    docker run --name myvotingapp -v /$(pwd)/build:/app -w //app -p 8080:80 -d ubuntu ./votingapp #-p es para mapear puertos de mi maquina virtual (de todos los containers) a mi container
-    #-d te devuelve el control del terminal
-    #popd    
+    docker run --name myvotingapp -p 8080:80 -d quimnadal/votingapp
+    #Runeo votingapp
 }
 
+retry(){
+    n=0
+    interval=5
+    retries=3
+    $@ && return 0
+    until [ $n -ge $retries ]
+    do
+        n=$[$n+1]
+        echo "Retrying...$n of $retries, wait for $interval seconds"
+        sleep $interval
+        $@ && return 0
+    done
+
+    return 1
+}
 
 #Test
 test(){
     curl --url http://localhost:8080/vote \
-        --request POST \ 
+        --request POST \
         --data  '{"topics":["dev", "ops"]}' \
         --header "Content-Type: application/json"
 
     curl --url http://localhost:8080/vote \
-        --request PUT \ 
-        --data  '{"topic":"dev"}'
+        --request PUT \
+        --data  '{"topic":"dev"}' \
         --header "Content-Type: application/json"
 
-    winner=(curl --url http://localhost:8080/vote \
-        --request DELETE \ 
-        --header "Content-Type: application/json" | jq -r '.winner')    #jq acepta lo de la izquierda y con el -r le envias una query sobre la propiedad de winner
+    winner=$(curl --url http://localhost:8080/vote \
+        --request DELETE \
+        --header "Content-Type: application/json" | jq -r '.winner')
 
     echo "Winner IS "$winner
 
@@ -62,6 +67,8 @@ test(){
 }
 
 deps 
-cleanup
+cleanup || true
 GOOS=linux build #compilo para linux porque voy a ejecutar esto en un container
-#retry test      #Arriba estara la funcion retry.
+retry test      #Arriba estara la funcion retry.
+
+docker push quimnadal/votingapp
--- test_pipeline.py
+++ test_pipeline.py
@@ -2,12 +2,12 @@ import requests
 import json
 
 url = 'http://localhost/vote'
-headers = "{'Content-type': 'application/json'}"
-data1 = "'topics':['dev', 'ops']"
-data2 = '{"topic":"dev"}'
+headers = {'Content-type': 'application/json'}
+data1 = {'topics':['dev', 'ops']}
+data2 = {"topic":"dev"}
 
 try:
-    request1=requests.post(url, data=data1, headers=headers)
+    request1=requests.post(url, data=data1, headers=headers) #pasar los datos como diccionario y luego pasarlo a json, no como string 
     request2=requests.put(url, data=data2, headers=headers)
     result=requests.get(url, ".winner", headers=headers)
     request3=requests.delete(url)
